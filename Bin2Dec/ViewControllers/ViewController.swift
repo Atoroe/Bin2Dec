7cb4923e9c842ea56a504c9294e82f0963b36419
Artiom P <apoluyanovich@gmail.com>
2021-04-19 00:26:05 +0300

    added converted logic

--- Bin2Dec/ViewControllers/ViewController.swift
+++ Bin2Dec/ViewControllers/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var mainStackView: UIStackView!
+    @IBOutlet weak var binTextField: UITextField!
+    @IBOutlet weak var decLabel: UILabel!
+    
     
     let startColor = UIColor(red: 255/255,
                              green: 224/255,
@@ -19,14 +22,39 @@ class ViewController: UIViewController {
                            green: 116/255,
                            blue: 116/255,
                            alpha: 1)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addGradient(startPointColor: startColor, endPointColor: endColor)
         view.bringSubviewToFront(mainStackView)
+        
+        
     }
-
-
+    
+    @IBAction func getAnswer() {
+        // create an integer array
+        guard let binChars = binTextField.text else { return }
+        var bins = [Int]()
+        
+        binChars.forEach { (value) in
+            bins.append(value.wholeNumberValue!)
+        }
+        
+        // convert bin to tec
+        var dec = Int()
+        var power = bins.count - 1
+        
+        for number in bins {
+            dec += number * NSDecimalNumber(decimal: pow(2, power)).intValue
+            power -= 1
+        }
+        
+        decLabel.text = String(dec)
+    }
+    
 }
+
+
+
 
