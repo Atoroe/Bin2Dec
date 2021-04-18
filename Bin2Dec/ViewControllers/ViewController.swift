bcaf53bc71754774e8083f22ff089f03e9f815fd
Artiom P <apoluyanovich@gmail.com>
2021-04-18 22:23:16 +0300

    setup menu

--- Bin2Dec/ViewControllers/ViewController.swift
+++ Bin2Dec/ViewControllers/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainStackView: UIStackView!
+    
+    let startColor = UIColor(red: 255/255,
+                             green: 224/255,
+                             blue: 68/255,
+                             alpha: 1)
+    let endColor = UIColor(red: 247/255,
+                           green: 116/255,
+                           blue: 116/255,
+                           alpha: 1)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addGradient(startPointColor: startColor, endPointColor: endColor)
+        view.bringSubviewToFront(mainStackView)
     }
 
 
